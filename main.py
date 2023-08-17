a747f984cd02acb7722497fe1c296f9c6f97d1e8
Mikk0git <kasprzakmilan1@gmail.com>
2023-08-18 00:47:09 +0200

    chord listener

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 from pynput import keyboard
 
+# Flaga wskazująca, czy klawisz jest wciśnięty
+key_pressed = False
+chord = ""
+
+
 def keyPressed(key):
-    print(str(key))
-    with open("keyfile.txt", 'a') as logKey:
-        try:
-            char = key.char
-            logKey.write(char)
-        except:
-            print("Error getting char")
+    global key_pressed
+    try:
+        key_pressed = True
+        char = key.char
+        print("Key pressed:", char)
+        global chord
+        chord = chord + char
+    except:
+        pass
+
+
+def keyReleased(key):
+    global key_pressed
+    try:
+        key_pressed = False
+        # char = key.char
+        # print("Key released:", char)
+    except:
+        pass
+
 
 if __name__ == "__main__":
-    listener = keyboard.Listener(on_press=keyPressed)
+    listener = keyboard.Listener(on_press=keyPressed, on_release=keyReleased)
     listener.start()
-    input()
+
+    while True:
+        if key_pressed:
+            print("Waiting for key release...")
+            while key_pressed:
+                pass
+            print("Key released. Waiting for key press... chord:", chord)
+            chord = ""
